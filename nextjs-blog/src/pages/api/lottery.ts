83f500837c91db10ed76e3d24dd900fa17966c2b
koyama-c <koyama@linkedge.jp>
2024-07-06 15:47:37 +0900

    おみくじAPIの作成

---
+++ nextjs-blog/src/pages/api/lottery.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export type LotteryResult = {
+  result: string;
+  fortune: number;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<LotteryResult>
+) {
+  const fortune = Math.random();
+  let result = "unlucky";
+  if (fortune >= 0.5) {
+    result = "lucky";
+  }
+  if (fortune >= 0.75) {
+    result = "very lucky";
+  }
+  res.status(200).json({ result, fortune });
+}
